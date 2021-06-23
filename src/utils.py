b4e7ba7cf9860cd402d5a639a16d346b2253f7b7
aditeyabaral <aditeya.baral@gmail.com>
2021-06-23 11:53:23 +0530

    increased delays

--- src/utils.py
+++ src/utils.py
@@ -196,24 +196,24 @@ async def generateSpongebobMeme(query):
 
 async def getPESUAnnouncements(chrome, username, password):
     chrome.get("https://pesuacademy.com/Academy")
-    await asyncio.sleep(2)
+    time.sleep(2)
 
     username_box = chrome.find_element_by_xpath(r'//*[@id="j_scriptusername"]')
     password_box = chrome.find_element_by_xpath(r'//*[@name="j_password"]')
 
     username_box.send_keys(username)
-    await asyncio.sleep(0.3)
+    time.sleep(0.3)
     password_box.send_keys(password)
-    await asyncio.sleep(0.3)
+    time.sleep(0.3)
 
     sign_in_button = chrome.find_element_by_xpath(
         r'//*[@id="postloginform#/Academy/j_spring_security_check"]')
     sign_in_button.click()
-    await asyncio.sleep(1)
+    time.sleep(1)
 
     menu_options = chrome.find_elements_by_xpath(r'//*[@class="menu-name"]')
     menu_options[8].click()
-    await asyncio.sleep(1)
+    time.sleep(1)
 
     announcement_boxes = chrome.find_elements_by_xpath(
         r'//*[@class="elem-info-wrapper"]')
@@ -246,7 +246,7 @@ async def getPESUAnnouncements(chrome, username, password):
                 all_attachments.extend(attachment_names)
                 for a in attachments:
                     a.click()
-                    await asyncio.sleep(0.5)
+                    time.sleep(0.5)
 
         img_base64 = None
         img_box = a_box.find_elements_by_xpath(
