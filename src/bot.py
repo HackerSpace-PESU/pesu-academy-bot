d7bead57d165d2ad847d5cb92c0c9de30d066726
aditeyabaral <aditeya.baral@gmail.com>
2021-07-15 11:56:10 +0530

    test reddit

--- src/bot.py
+++ src/bot.py
@@ -304,18 +304,22 @@ async def on_ready():
 
     if not checkNewDay.is_running():
         checkNewDay.start()
+        time.sleep(0.3)
 
     if not changeStatus.is_running():
         changeStatus.start()
+        time.sleep(0.3)
 
     if not checkRedditPost.is_running():
         checkRedditPost.start()
+        time.sleep(0.3)
 
-    '''if not checkInstagramPost.is_running():
+    if not checkInstagramPost.is_running():
         checkInstagramPost.start()
+        time.sleep(0.3)
 
     if not checkPESUAnnouncement.is_running():
-        checkPESUAnnouncement.start()'''
+        checkPESUAnnouncement.start()
 
 
 @client.event
